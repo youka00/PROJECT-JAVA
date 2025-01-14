54c3860a6e17db0470a93a64912e75001f239bb7
Youka <112777620+youka00@users.noreply.github.com>
2025-01-15 00:26:50 +0200

    start

---
+++ Contact.java
@@ -0,0 +1,17 @@
+import java.io.Console;
+
+public class Contact{
+    public static void main(String[] args) {
+        Person person = addNewPerson();
+        person.printPerson();
+        person.info();
+        person.edit();
+        person.delete();
+
+    }
+    public Person addNewPerson() {
+        
+    }
+
+    
+}
